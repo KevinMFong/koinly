1f3884a8b9fad8454734a7f13a3a149e4df90296
Kevin Fong <22036173+KevinMFong@users.noreply.github.com>
2022-12-31 17:08:53 -0500

    fix: handle duplicate ids when processing transactions

--- src/koinly/process_transactions.py
+++ src/koinly/process_transactions.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import typer
 
 import csv
@@ -62,53 +64,60 @@ def process_transactions():
     Process raw transaction data.
     """
     typer.echo("Processing raw transaction data.")
-    raw_transactions = []
+    raw_transactions_map = defaultdict(list)
     bronze_files = DataDirectory.BRONZE.value.joinpath("koinly_transactions").glob("**/*.json")
     for bronze_file in bronze_files:
         typer.echo(f"Processing {bronze_file.parent.name}/{bronze_file.name}")
         with open(bronze_file) as file:
             file_rows = file.readlines()
         for row in file_rows:
-            raw_transactions.extend(json.loads(row)["transactions"])
+            raw_transactions_map[f"{bronze_file.parent.name}"].extend(json.loads(row)["transactions"])
 
-    report_transactions = []
+    report_transactions_map = defaultdict(list)
     schema = ReportTransactionSchema()
-    for raw_txn in raw_transactions:
-        txn = {}
-        if raw_txn["ignored"] is True:
-            continue
-        txn["id"] = raw_txn["id"]
-        txn["date"] = str(parse(raw_txn["date"]))
-        txn["type"] = raw_txn["type"]
-        txn["label"] = raw_txn["label"]
+    for date_partition, raw_transactions in raw_transactions_map.items():
+        for raw_txn in raw_transactions:
+            txn = {}
+            if raw_txn["ignored"] is True:
+                continue
+            txn["id"] = raw_txn["id"]
+            txn["date"] = str(parse(raw_txn["date"]))
+            txn["type"] = raw_txn["type"]
+            txn["label"] = raw_txn["label"]
 
-        sending_wallet, sent_amount, sent_currency, sent_cost_basis = _get_from_data(raw_txn)
-        txn["sending_wallet"] = sending_wallet
-        txn["sent_amount"] = sent_amount
-        txn["sent_currency"] = sent_currency
-        txn["sent_cost_basis"] = sent_cost_basis
+            sending_wallet, sent_amount, sent_currency, sent_cost_basis = _get_from_data(raw_txn)
+            txn["sending_wallet"] = sending_wallet
+            txn["sent_amount"] = sent_amount
+            txn["sent_currency"] = sent_currency
+            txn["sent_cost_basis"] = sent_cost_basis
 
-        receiving_wallet, received_amount, received_currency, received_cost_basis = _get_to_data(raw_txn)
-        txn["receiving_wallet"] = receiving_wallet
-        txn["received_amount"] = received_amount
-        txn["received_currency"] = received_currency
-        txn["received_cost_basis"] = received_cost_basis
+            receiving_wallet, received_amount, received_currency, received_cost_basis = _get_to_data(raw_txn)
+            txn["receiving_wallet"] = receiving_wallet
+            txn["received_amount"] = received_amount
+            txn["received_currency"] = received_currency
+            txn["received_cost_basis"] = received_cost_basis
 
-        fee_amount, fee_currency = _get_fee_data(raw_txn)
-        txn["fee_amount"] = fee_amount
-        txn["fee_currency"] = fee_currency
-        txn["fee_value_usd"] = raw_txn["fee_value"]
+            fee_amount, fee_currency = _get_fee_data(raw_txn)
+            txn["fee_amount"] = fee_amount
+            txn["fee_currency"] = fee_currency
+            txn["fee_value_usd"] = raw_txn["fee_value"]
 
-        txn["gain_usd"] = raw_txn["gain"]
-        txn["net_value_usd"] = raw_txn["net_value"]
-        txn["tx_src"] = raw_txn["txsrc"]
-        txn["tx_dest"] = raw_txn["txdest"]
-        txn["tx_hash"] = raw_txn["txhash"]
-        txn["description"] = raw_txn["description"]
+            txn["gain_usd"] = raw_txn["gain"]
+            txn["net_value_usd"] = raw_txn["net_value"]
+            txn["tx_src"] = raw_txn["txsrc"]
+            txn["tx_dest"] = raw_txn["txdest"]
+            txn["tx_hash"] = raw_txn["txhash"]
+            txn["description"] = raw_txn["description"]
 
-        report_transaction = schema.load(txn)
-        report_transactions.append(report_transaction)
+            report_transaction = schema.load(txn)
+            report_transactions_map[date_partition].append(report_transaction)
 
+    # De-dup transactions by transaction id, keeping transaction from most recent date partition.
+    transaction_ids_by_date_parition_map = {}
+    for key in sorted(report_transactions_map.keys()):
+        transaction_ids_by_date_parition_map |= {txn.id: txn for txn in report_transactions_map[key]}
+
+    report_transactions = sorted(transaction_ids_by_date_parition_map.values(), key=lambda txn: txn.date)
     with open(
         DataDirectory.SILVER.value.joinpath("koinly_transactions", "transactions_report.json"), "w"
     ) as file:
