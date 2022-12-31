0b55580ae7ecae5509eef7ac492f71eb3ef681c2
Kevin Fong <22036173+KevinMFong@users.noreply.github.com>
2022-12-31 12:12:26 -0500

    feat: add ReportTransaction and ReportTransactionSchema

---
+++ src/koinly/transaction.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from typing import NamedTuple, Optional, Any
+
+from marshmallow import Schema, fields, post_load
+
+__all__ = (
+    "ReportTransaction",
+    "ReportTransactionSchema",
+)
+
+
+class ReportTransaction(NamedTuple):
+    date: datetime
+    net_value_usd: float
+    type: str  # TODO convert to enum
+    description: Optional[str] = None
+    fee_amount: Optional[float] = None
+    fee_currency: Optional[str] = None
+    fee_value_usd: Optional[float] = None
+    gain_usd: Optional[float] = None
+    label: Optional[str] = None  # TODO convert to enum
+    received_amount: Optional[float] = None
+    received_cost_basis: Optional[float] = None
+    received_currency: Optional[str] = None
+    receiving_wallet: Optional[str] = None
+    sending_wallet: Optional[str] = None
+    sent_amount: Optional[float] = None
+    sent_cost_basis: Optional[float] = None
+    sent_currency: Optional[str] = None
+    tx_dest: Optional[str] = None
+    tx_hash: Optional[str] = None
+    tx_src: Optional[str] = None
+
+
+class ReportTransactionSchema(Schema):
+    class Meta:
+        ordered = True
+
+    date = fields.DateTime(required=True)
+    type = fields.String(required=True)
+    label = fields.String(required=False, missing=None)
+    sending_wallet = fields.String(required=False, missing=None)
+    sent_amount = fields.Float(required=False, missing=None)
+    sent_currency = fields.String(required=False, missing=None)
+    sent_cost_basis = fields.Float(required=False, missing=None)
+    receiving_wallet = fields.String(required=False, missing=None)
+    received_amount = fields.Float(required=False, missing=None)
+    received_currency = fields.String(required=False, missing=None)
+    received_cost_basis = fields.Float(required=False, missing=None)
+    fee_amount = fields.Float(required=False, missing=None)
+    fee_currency = fields.String(required=False, missing=None)
+    gain_usd = fields.Float(required=False, missing=None)
+    net_value_usd = fields.Float(required=True)
+    fee_value_usd = fields.Float(required=False, missing=None)
+    tx_src = fields.String(required=False, missing=None)
+    tx_dest = fields.String(required=False, missing=None)
+    tx_hash = fields.String(required=False, missing=None)
+    description = fields.String(required=False, missing=None)
+
+    @post_load
+    def deserialize(self, data: dict, **kwargs: Any) -> Any:
+        return ReportTransaction(**data)
