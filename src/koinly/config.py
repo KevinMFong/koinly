6ad2a378e52969c0e1b0c0bb05858f4133f3c906
Kevin Fong <22036173+KevinMFong@users.noreply.github.com>
2022-12-31 12:09:03 -0500

    feat: add config.py

---
+++ src/koinly/config.py
@@ -0,0 +1,11 @@
+import enum
+from pathlib import Path
+
+_DATA_ROOT_PATH = Path(__file__).parent.parent.parent.joinpath("data")
+
+
+class DataDirectory(enum.Enum):
+
+    BRONZE = _DATA_ROOT_PATH.joinpath("0_bronze")
+    SILVER = _DATA_ROOT_PATH.joinpath("1_silver")
+    GOLD = _DATA_ROOT_PATH.joinpath("2_gold")
