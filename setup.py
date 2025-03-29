24dd1b9e60d133d9b74153af12d2cc06c93db697
Kevin Fong <22036173+KevinMFong@users.noreply.github.com>
2025-03-29 15:27:33 -0700

    fixup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: End Users/Desktop",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.12",
     ],
     description="Koinly command line interface.",
     install_requires=[
